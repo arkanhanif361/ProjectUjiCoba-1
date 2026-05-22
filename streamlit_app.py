b13b885382f204069b1388c23c7a6c60bde3f762
arkanhanif361 <m.arkanhanif18@gmail.com>
2026-05-22 14:13:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 Project Kelas A")
+st.title("🎈 EEEEE A")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
